0004a617dc37a62a3a6f59454811f71cc497261f
rachel <rachel058326@gmail.com>
2023-09-28 14:06:46 +0300

    it works

--- Documents/chat-app/scripts/deploy.sh
+++ Documents/chat-app/scripts/deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-version=$1
-commit_hash=$2
-tag_and_push=$3
 
-if [[ -z $version && -z $commit_hash && -z $tag_and_push ]]; then
-    echo "Missing parameters"
-    exit 1
-fi
+read -p "Enter version: " version
+git log --oneline
+read -p "Enter commit hash: " commit_hash
 
 image_name=chat-app:${version}
 
@@ -14,31 +10,32 @@ image_name=chat-app:${version}
 if [[ "$(docker images -q $image_name)" ]]; then
     read -p "Image $image_name already exists. Do you want to rebuild it (y/n)? " rebuild
     # If the user chooses to rebuild the image, delete the existing one.
-            if [[ "$rebuild" == "y" ]]; then
-            # Delete the existing image
-            docker rmi "chat-app:$version"
-            else
-            echo "Using existing image chat-app:$version."
-            fi
+    if [[ "$rebuild" == "y" ]]; then
+        # Delete the existing image
+        docker rmi "chat-app:$version"
+        docker build -t "chat-app:$version" .
+    else
+         echo "Using existing image chat-app:$version."
+    fi
 
 
 else
     # Build the Docker image
     docker build -t "chat-app:$version" .
 fi
+read -p "Do you want to push  tag to git: " tag_and_push
+
 if [[ $tag_and_push == "true" ]]; then
         git tag  $version $commit_hash
         git push origin $version
 fi
 
 
-
    read -p "Do you want to push the image to Artifact Registry? (y/n): " push_to_registry
     if [[ "$push_to_registry" == "y" ]]; then
-      gcloud auth login
       gcloud config set auth/impersonate_service_account artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com  
       gcloud auth configure-docker me-west1-docker.pkj.dev
-      docker tag chat-app:${version} me-west1-docker.pkg.dev/grunitech-mid-project/rachel-gershon-chat-app-images/chat-app:${version}
+      docker tag chat-app:${version} me-west1-docker.pkg.dev/grunitech-mid-project/	 rachel-gershon-chat-app-images/chat-app:${version}
       docker push me-west1-docker.pkg.dev/grunitech-mid-project/rachel-gershon-chat-app-images/chat-app:${version}
     else
         echo "Image not pushed to Artifact Registry."
